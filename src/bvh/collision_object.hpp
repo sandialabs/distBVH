29c664854cf971baf88066632ce65aa6c8392e51
Cezary Skrzyński <cezary.skrzynski@ng-analytics.com>
2023-12-15 14:20:08 +0100

    fix access specifiers

--- src/bvh/collision_object.hpp
+++ src/bvh/collision_object.hpp
@@ -230,8 +230,6 @@ namespace bvh
     impl &get_impl() noexcept { return *m_impl; }
     const impl &get_impl() const noexcept { return *m_impl; }
 
-  private:
-
     template< typename T, typename... ViewProp >
     void
     update_snapshots( Kokkos::View< const T *, ViewProp... > _data_view )
@@ -258,6 +256,8 @@ namespace bvh
           snap( _idx ) = make_snapshot( _data_view( _idx ), static_cast< std::size_t >( _idx ) );
         } );
     }
+
+  private:
 
     void for_each_tree_impl( tree_function &&_fun );
     void for_each_result_impl( std::function< void(const narrowphase_result &) > &&_fun );
