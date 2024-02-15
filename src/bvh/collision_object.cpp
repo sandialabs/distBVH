1ef762c777fef2e062c4fd42886ce41c1bc912d0
Cezary Skrzyński <cezary.skrzynski@ng-analytics.com>
2024-02-15 14:08:19 +0100

    convert narrowphase - add FIXME_CUDA markers

--- src/bvh/collision_object.cpp
+++ src/bvh/collision_object.cpp
@@ -132,6 +132,7 @@ namespace bvh
       const auto send = ( i == m_impl->num_splits ) ? m_impl->split_indices_h.extent( 0 ) : m_impl->splits_h( i );
       const std::size_t nelements = send - sbeg;
       ::bvh::vt::debug( "{}: creating broadphase patch for body {} size {} from offset {}\n", ::vt::theContext()->getNode(), m_impl->collision_idx, nelements, sbeg );
+      // FIXME_CUDA
       m_impl->local_patches[i] = broadphase_patch_type(
         i + rank * od_factor, span< const entity_snapshot >( m_impl->snapshots.data() + sbeg, nelements ) );
     }
