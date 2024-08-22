2045c6cf1c804475214f33503ee56f53f7f53e06
Nicolas Morales <nmmoral@sandia.gov>
2024-08-22 12:34:47 -0400

    fix error in package file

--- spack-repos/p3a/packages/p3a/package.py
+++ spack-repos/p3a/packages/p3a/package.py
@@ -16,9 +16,9 @@ class P3a(CMakePackage):
         args = ["-Dmpicpp_ROOT={}".format(self.spec["mpicpp"].prefix),
                 "-Dkokkos_ROOT={}".format(self.spec["kokkos"].prefix)]
 
-        if spec.satisfies("^kokkos+rocm"):
-            args.append("-DCMAKE_CXX_COMPILER={}".format(spec["hip"].hipcc))
+        if self.spec.satisfies("^kokkos+rocm"):
+            args.append("-DCMAKE_CXX_COMPILER={}".format(self.spec["hip"].hipcc))
         else:
             # Compiler weirdness due to nvcc_wrapper
-            args.append("-DCMAKE_CXX_COMPILER={}" .format(spec["kokkos"].kokkos_cxx))
+            args.append("-DCMAKE_CXX_COMPILER={}" .format(self.spec["kokkos"].kokkos_cxx))
         return args
